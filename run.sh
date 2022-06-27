78375927b47cdff55768848620fae7be1dcf5d76
wjh720 <1040594377@qq.com>
2022-06-27 13:46:26 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -52,7 +52,7 @@
  --is_offline_pearl=${is_offline_pearl} \
  --allow_backward_z=${allow_backward_z} \
  >& out_logs/${Foldername}/${task}_${algo}_${datadir}_${is_sparse}_${use_brac}_${use_information_bottleneck}_${is_zloss}_${is_onlineadapt_uniform}_${is_offline_pearl}_${allow_backward_z}_${seed}.txt &
- echo "task: ${task}, algo: ${algo}, datadir: ${datadir}, is_spars: ${is_sparse}, use_information_bottleneck: ${use_information_bottleneck}"
+ echo "task: ${task}, algo: ${algo}, datadir: ${datadir}, is_sparse: ${is_sparse}, use_information_bottleneck: ${use_information_bottleneck}"
  echo "     is_zloss: ${is_zloss}, is_onlineadapt_uniform: ${is_onlineadapt_uniform}, is_offline_pearl: ${is_offline_pearl}"
  echo "     allow_backward_z: ${allow_backward_z}, seed: ${seed}, GPU: $n"
  n=$[($n+1) % ${gpunum}]
